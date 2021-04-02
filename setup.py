12d2ff6b395916b6670b5d4f251a6630fad65069
tinkerer_ <v.i.sluiter@saxion.nl>
2021-04-02 22:40:10 +0200

    Added higher version number in setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 setuptools.setup(
      name='adr-tools-python',  
-     version='1.0.3',
+     version='1.0.4b',
      entry_points = {
         'console_scripts':[
             'adr-init = adr_init:main',
